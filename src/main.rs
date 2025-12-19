748ba65855c5b38c9e549d36c64b4114d3a86fe7
Michael VanBemmel <mvanbem@google.com>
2025-12-19 11:18:24 -0800

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 fn main() {
     println!("Panicking locally");
     std::panic::catch_unwind(|| {
-	panic!("panic locally");
-    }).unwrap_err();
+        panic!("panic locally");
+    })
+    .unwrap_err();
 
     println!("Panicking in core");
     std::panic::catch_unwind(|| {
-	let mut buf = [0u8; 1];
-	buf.copy_from_slice(&[1, 2]);
-    }).unwrap_err();
+        let mut buf = [0u8; 1];
+        buf.copy_from_slice(&[1, 2]);
+    })
+    .unwrap_err();
 }
